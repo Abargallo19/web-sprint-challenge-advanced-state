4e987b0c9013ccefea45a6fd74af0f137151ecd2
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-08-01 09:03:12 -0400

    fixed bugs in action creators

--- frontend/components/Message.js
+++ frontend/components/Message.js
@@ -1,5 +1,15 @@
-import React from 'react'
 
-export default function Message(props) {
-  return <div id="message">Nice job!</div>
+import React, {useEffect, useState} from 'react'
+import { connect } from 'react-redux'
+
+function Message(props) {
+  return <div id="message">{props.message}</div>
 }
+
+const mapStateToProps = (state) => {
+  return({
+    message: state.infoMessage
+  })
+}
+
+export default connect(mapStateToProps, {})(Message)
